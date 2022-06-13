6241d3d9ea71296554573d725ea17072054dba9c
Leandro <104011760+LeoJNF@users.noreply.github.com>
2022-06-13 14:01:48 -0300

    Update exe004.py

--- exe004.py
+++ exe004.py
@@ -1,3 +1,15 @@
-n1 = int(input('\033[7;40mDigite um numero: '))
-n2 = int(input('\033[7;40mDigite outro numero: '))
-print('\033[7;40mA soma de {} com {} vale {}\033[m'.format(n1, n2, n1 + n2))
+print('TRIANGULOS')
+tri = []
+
+for n in range(3):
+    tri.append(int(input(f'Informe o {n+1}° lado: ')))
+if tri[0] + tri[1] > tri[2] and tri[1] + tri[2] > tri[0] and tri[0] + tri[2] > tri[1] :
+    print('é um triangulo', end=' ')
+    if tri[0] != tri[1] and tri[0] != tri[2] and tri[1] != tri[2]:
+        print('Escaleno')
+    elif tri[0] == tri[1] and tri[0] == tri[2]:
+        print('Equilátero')
+    else:
+        print('Isóceles')
+else:
+    print('não é um triângulo')
