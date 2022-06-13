2d69004a59c8f834f82554d807e41c0fce00e0f3
Leandro <104011760+LeoJNF@users.noreply.github.com>
2022-06-13 14:03:48 -0300

    Update exe006.py

--- exe006.py
+++ exe006.py
@@ -1,2 +1,23 @@
-n = int(input('digite um numero: '))
-print('o numero {} tem seu antecessor {} e seu sucessor {}'.format(n, (n-1), (n+1)))
+lista = []
+print('Insira quantos valor quiser!!')
+print('obs. -1 para parar')
+while True:
+    lista.append(int(input('informe aqui --> ')))
+    if -1 in lista:
+        break
+lista.pop(len(lista)-1)
+print(f'Foi informado {len(lista)} numeros.')
+print(f'Os numeros informados: {lista}')
+lista.reverse()
+print(f'A ordem inversa {lista}')
+print(f'A soma dos valores {sum(lista)}')
+print(f'A média dos valores {sum(lista)/len(lista):.2f}')
+print('Numeros informados acimda da média: ', end=' ')
+for n in lista:
+    if n > sum(lista)/len(lista):
+        print(n, end=' ')
+print('\nNumeros abaixo de sete:', end=' ')
+for n in lista:
+    if n < 7:
+        print(n, end=' ')
+print('\nVALEU PAIZAO. Programa acabou!!')
