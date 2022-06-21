8fd021f4631a8b3d074fa6d7fc7ae4e3f31dba78
Leandro <104011760+LeoJNF@users.noreply.github.com>
2022-06-21 19:44:22 -0300

    Update exe053.py

--- exe053.py
+++ exe053.py
@@ -1,11 +1,8 @@
 num = int(input('Digite um numero: '))
-total = 0
-for c in range(1, num+1):
-    if num % c == 0:
-        total = total + 1
+total2 = 0
+total2 = sum([total2+1 for c in range(1, num+1) if num % c == 0])
 print('o numero {} foi divisivel {} vezes.'.format(num, total))
 if total == 2:
     print('E por isso ele é um numero primo.')
 else:
     print('E por isso ele não é um numero primo.')
-
