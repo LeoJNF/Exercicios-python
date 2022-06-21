d9aeef57a6bc733785d2b137ef3e51022de1b98e
Leandro <104011760+LeoJNF@users.noreply.github.com>
2022-06-21 19:28:15 -0300

    Update exe052.py

--- exe052.py
+++ exe052.py
@@ -1,5 +1,5 @@
 num = int(input('escolha o primeiro termo: '))
 num2 = int(input('escolha a razão: '))
 decimo = num + (10-1) * num2
-for c in range(num, decimo + num2, num2):
-    print('{}'.format(c), end='-')
+[print(f'{c}', end=' - ') for c in range(num, decimo + num2, num2)]
+print('FIM')
